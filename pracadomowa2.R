b16a9cc92450a489131d4ca50dacfb9a9ffc5927
Mateusz Chojnacki <mateuszchojnacki@Mateuszs-MacBook-Pro.local>
2018-11-07 22:16:10 +0100

    cw 4

--- pracadomowa2.R
+++ pracadomowa2.R
@@ -1,4 +1,4 @@
-#zad1 Utworz list?? temp z??ozon?? z siedmiu element??w. Ka??dy element jest wektorem z??o??onym z 5 losowych liczb rzeczywistych (postaraj si?? u??y?? funkcji lapply).
+# zad1 Utworz list?? temp z??ozon?? z siedmiu element??w. Ka??dy element jest wektorem z??o??onym z 5 losowych liczb rzeczywistych (postaraj si?? u??y?? funkcji lapply).
 createVec <- function(count){
   sample(1:25, 5, rep=FALSE)
 }
@@ -6,25 +6,36 @@ temp <- lapply(1:7, createVec)
 
 # Do listy temp do????cz nazwy dni tygodnia. 
 temp[[length(temp)+1]] <- c("Pon", "Wt","Sr","Czw","Pi","So","Nie")
-#Dla listy temp
-#zad1a Wygeneruj wektor z minimalnymi temperaturami ka??dego dnia.
+# Dla listy temp
+# zad1a Wygeneruj wektor z minimalnymi temperaturami ka??dego dnia.
 sapply(temp[1:7], FUN= min)
 
 tabela.danych <- data.frame(sapply(temp[1:7], FUN= min),temp[8])
 names(tabela.danych)<- c("min temp","dzien")
-#zad1b Wygeneruj wektor z maksymalnymi temperaturami ka??dego dnia.
+# zad1b Wygeneruj wektor z maksymalnymi temperaturami ka??dego dnia.
 sapply(temp[1:7], FUN= max)
 
 tabela.danych <- data.frame(sapply(temp[1:7], FUN= max),temp[8])
 names(tabela.danych)<- c("max temp","dzien")
 
-#zad2 Wczytaj zbi??r USArrests.csv
-#https://raw.githubusercontent.com/vincentarelbundock/Rdatasets/master/csv/datasets/USArrests.csv
+# zad2 Wczytaj zbi??r USArrests.csv
+
 ds <- read.csv("https://raw.githubusercontent.com/vincentarelbundock/Rdatasets/master/csv/datasets/USArrests.csv")
+
 # nast??pnie podaj wektor, kt??rego elementy to sumy przest??pstw ka??dego rodzaju.
+
 sapply(ds[2:5], sum)
 
-#zad3 Napisz funkcj??, kt??ra dla danego wektora zwr??ci warto??ci powy??ej ??redniej tego wektora.
+# zad3 Napisz funkcj??, kt??ra dla danego wektora zwr??ci warto??ci powy??ej ??redniej tego wektora.
 
+meanVec <- function(vec) {
+  return(mean(vec))
+}
+
+usedcars <- read.csv("https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/usedcars.csv")
+
+sapply(usedcars["price"], meanVec)
+
+sapply(usedcars["mileage"], meanVec)
 
 
